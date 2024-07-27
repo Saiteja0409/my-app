d38d3e2e0297b913117edd639e813c68539be25f
Sai Teja <eppapallyteja@gmail.com>
2024-07-27 14:37:43 +0530

    Nested Form Group

--- src/app/create-form/create-form.component.ts
+++ src/app/create-form/create-form.component.ts
@@ -12,13 +12,20 @@ export class CreateFormComponent implements OnInit {
     name: new FormControl(),
     age: new FormControl(),
     phone: new FormControl(),
-    email: new FormControl()
+    email: new FormControl(),
+    address: new FormGroup({
+      city: new FormControl(),
+      pincode: new FormControl()
+    })
 
   })
 
   constructor() { }
 
   ngOnInit(): void {
+  }
+  submit(){
+    console.log(this.userForm);
   }
 
 }
